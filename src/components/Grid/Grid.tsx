72f4e56b97fae017e0efba531c776cd01acded9b
Ash Bars <barsegyan@topsystems.ru>
2023-04-13 12:34:03 +0300

    feat: -добавить панель фильтра для Grid, -изменить несколько свойств компоненты через метод options, с параметром типа объект.
    (смена нескольких свойств одновременно)

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,11 +1,12 @@
-import React, {ReactNode, useRef, useState} from 'react';
+import React, {ReactNode, useRef} from 'react';
 
 import DataGrid, {
-    Scrolling, Paging, Column, HeaderFilter,
+    Scrolling, Paging, Column, HeaderFilter, FilterRow, FilterPanel, FilterBuilderPopup, ColumnChooser,
 } from 'devextreme-react/data-grid';
 import * as AspNetData from 'devextreme-aspnet-data-nojquery';
 
-import { on } from "devextreme/events";
+import {on} from "devextreme/events";
+import {Button} from "devextreme-react";
 
 const dataSource = AspNetData.createStore({
     key: 'Id',
@@ -13,7 +14,7 @@ const dataSource = AspNetData.createStore({
 });
 
 
-type TGridProps = {children?: ReactNode}
+type TGridProps = { children?: ReactNode }
 
 const columnsInfo = ['Id', 'StoreName', 'ProductCategoryName', 'ProductName', 'DateKey', 'SalesAmount'];
 
@@ -22,40 +23,59 @@ const cellRenderTemplate = (cellData: any) => {
     return <>{cellData.displayValue}</>
 }
 
-export const Grid = (props: TGridProps) =>  {
+export const Grid = (props: TGridProps) => {
 
+    const filterBuilderPopupPosition = {
+        of: window,
+        at: "center",
+        my: "center",
+        // offset: { y: 20 }
+    };
+
+    const getGridInfo = () => {
+        if (gridRef.current) {
+           gridRef.current.instance.option({
+               filterValue: [],
+               width:'90%'
+           });
+        }
+    };
+
+    const gridRef = useRef<DataGrid>(null);
     const columnsJSX = columnsInfo.map((c) => {
-            return <Column dataField={c} caption={c + 'test'} cellRender={cellRenderTemplate}/>
+        return <Column dataField={c} caption={c + 'test'} cellRender={cellRenderTemplate}/>
     })
-    const gridRef = useRef(null);
+    return (<>
+            <Button text={'Get Info'} onClick={getGridInfo}/>
+            <DataGrid
+                ref={gridRef}
+                height={440}
+                dataSource={dataSource}
+                showBorders={true}
+                remoteOperations={true}
+                wordWrapEnabled={false}
+                allowColumnResizing
+                cellHintEnabled
+                onCellPrepared={(e) => {
+                    on(e.cellElement, "mouseover", (arg: any) => {
+                        if (e.cellElement.offsetWidth < e.cellElement.scrollWidth) {
+                            e.cellElement.title = e.displayValue;
+                        }
+                    })
+                }}
+            >
+                <FilterRow visible={true}/>
+                <FilterPanel visible={true}/>
+                <FilterBuilderPopup position={filterBuilderPopupPosition}/>
+                <ColumnChooser enabled mode={'select'}/>
 
-
-        return (<>
-                <DataGrid
-                    ref={gridRef}
-                    height={440}
-                    dataSource={dataSource}
-                    showBorders={true}
-                    remoteOperations={true}
-                    wordWrapEnabled={false}
-                    allowColumnResizing
-                    cellHintEnabled
-                    onCellPrepared={(e) => {
-                        on(e.cellElement, "mouseover", (arg: any) => {
-                            if (e.cellElement.offsetWidth < e.cellElement.scrollWidth) {
-                                e.cellElement.title = e.displayValue;
-                                console.log('test')
-                            }
-                        })
-                    }}
-                >
-                    <Scrolling mode="virtual" rowRenderingMode="virtual" />
-                    <Paging defaultPageSize={100} />
-                    <HeaderFilter visible={true} allowSearch={true} />
-                    {columnsJSX}
-                </DataGrid>
+                <Scrolling mode="virtual" rowRenderingMode="virtual"/>
+                <Paging defaultPageSize={100}/>
+                <HeaderFilter visible={true} allowSearch={true}/>
+                {columnsJSX}
+            </DataGrid>
         </>
-        );
+    );
 }
 
 
