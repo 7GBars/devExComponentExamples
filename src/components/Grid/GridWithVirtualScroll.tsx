48632e9bfde36d41e4994a7caf64d6660b0b0e3f
Ash Bars <barsegyan@topsystems.ru>
2023-04-28 16:45:22 +0300

    feat: -добавить virtual scroll,  remoteOperations === true

--- src/components/Grid/GridWithVirtualScroll.tsx
+++ src/components/Grid/GridWithVirtualScroll.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DataGrid from "devextreme-react/data-grid";
+import DataGrid, {Scrolling, Paging, Pager} from "devextreme-react/data-grid";
 
 import {AspNetDataSource} from "../../1_stores/AspnNetDataStore/AspNetStore";
 import {TCommonProps} from "../../2_commonTypes/componentCommonTypes";
@@ -12,7 +12,17 @@ export function GridWithVirtualScroll(props: TCommonProps) {
             <DataGrid
                 dataSource={AspNetDataSource}
                 height={440}
-            />
+                showBorders={true}
+                remoteOperations={true}
+                wordWrapEnabled={true}
+            >
+                <Scrolling mode="virtual" />
+                <Paging defaultPageSize={100} />
+                <Pager
+                    visible={true}
+                    showPageSizeSelector
+                />
+            </DataGrid>
         </div>
     );
 }
