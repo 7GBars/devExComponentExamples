838b8dd483a06a060b1474540e02e1dbd9a44e3e
Ash Bars <barsegyan@topsystems.ru>
2023-06-07 18:20:59 +0300

    feat: -установка mobx, -шаблон для DialogStore

---
+++ src/0_State/DialogsStore.ts
@@ -0,0 +1,24 @@
+import uuid4 from "uuid4";
+import {makeAutoObservable} from "mobx";
+type TDialogPayload = {title: string; body: any};
+type TDialogInfo = { isOpen: boolean; dialogPayload: TDialogPayload}
+type TDialogs = {
+  [key: string]: TDialogInfo
+}
+export class DialogsStore {
+  public dialogs: TDialogs;
+  public createDialogId() {
+    return uuid4();
+  }
+  constructor() {
+    this.dialogs = {}
+    makeAutoObservable(this);
+  }
+  public openDialog(dialogId: string) {
+    this.dialogs &&( this.dialogs[dialogId].isOpen = true );
+  }
+  public closeDialog(dialogId: string) {
+    this.dialogs && ( this.dialogs[dialogId].isOpen = false );
+    delete this.dialogs[dialogId];
+  }
+}
