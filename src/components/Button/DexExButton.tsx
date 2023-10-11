e54fa86f324c891ca722cb913f4e3d82254cff30
Ash Bars <barsegyan@topsystems.ru>
2023-10-11 12:17:17 +0300

    -добавить колбек который меняет содержимое компоненты render айтэмся тулбара

--- src/components/Button/DexExButton.tsx
+++ src/components/Button/DexExButton.tsx
@@ -6,12 +6,17 @@ type TDexExButtonProps = Partial<IButtonOptions>;
 
 function DexExButton(props: TDexExButtonProps & TTemplateProps) {
   const buttonRef = useRef<Button>(null);
+  const icon = props.icon;
+
+  console.log('render button component', buttonRef.current?.instance.option(''));
   return (
     <>
       <Button
+        onInitialized={(e) => {
+          e.component?.option('icon', props.icon)
+        }}
         ref={buttonRef}
         id={'test'}
-        icon={props.icon}
         hint={props.text}
         text={props.text}
         onClick={props.onClick}
--- src/components/Toolbar/DevExToolBar.tsx
+++ src/components/Toolbar/DevExToolBar.tsx
@@ -4,19 +4,46 @@ import {Item} from "devextreme-react/toolbar";
 import Guid from "devextreme/core/guid";
 import './DevExToolBar.scss';
 import DexExButton from "../Button/DexExButton";
+import {ClickEvent} from "devextreme/ui/button";
 
-const toolbarItemsInfo = [
-  {text: 'Добавить', icon: 'add', onClick: (e: any) => console.log(e.component.option('icon')), id: new Guid()},
-  {text: 'Удалить', icon: 'remove', onClick: (e: any) => console.log(e.component.option('icon')), id: new Guid()},
-  {text: 'Изменить', icon: 'edit', onClick: (e: any) => console.log(e.component.option('icon')), id: new Guid()}
-] as const;
+
 
 export function DevExToolBar(props: any) {
+  const toolbarItemsInfo = [
+    { text: 'Добавить',
+      icon: 'add',
+      onClick: (e: any) => {
+
+      },
+      id: new Guid()},
+    { text: 'Удалить',
+      icon: 'remove',
+      onClick: (e: ClickEvent) => {
+      const randomValue = Math.round(Math.random());
+        e.component.option('icon', randomValue > 0 ? 'add' : 'remove' );
+      },
+      id: new Guid()},
+    { text: 'Изменить',
+      icon: 'edit',
+      onClick: (e: any) => {
+        console.log(e.component.option('icon'))
+      },
+      id: new Guid()
+    }
+  ] as const;
+
+
   return (
     <div>
-      <Toolbar onContentReady={(e) => console.log('toolbar content ready')}>
+      <Toolbar
+        onContentReady={(e) => console.log('toolbar content ready')}
+      >
         {toolbarItemsInfo.map(i => {
-          return <Item key={i.id.toString()} widget="dxButton" render={(e) => <DexExButton {...e} {...i}/>}/>
+          return <Item
+            key={i.id.toString()}
+            widget="dxButton"
+            render={(e) => <DexExButton {...e} {...i}/>}
+          />
         })}
       </Toolbar>
     </div>
