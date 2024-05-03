e32219e10f3db358eb9de2587a9161cc9c17602c
Barsegyan <barsegyan@topsystems.ru>
2024-05-03 16:45:39 +0300

    -реализовать свитчер тем

--- src/HOCs/MainLayoutWrapper.tsx
+++ src/HOCs/MainLayoutWrapper.tsx
@@ -1,19 +1,29 @@
 import React, {FC, PropsWithChildren} from 'react';
 import {Button} from "devextreme-react/button";
+import DevextremeUiThemes from "devextreme/ui/themes";
 
 type TMainLayoutWrapperProps = PropsWithChildren<{
   displayName: string;
 }>;
-
+const themes = {
+  base: "generic.light",
+  dark: "generic.dark"
+};
+const themeMap = {
+  [themes.base]: themes.dark,
+  [themes.dark]: themes.base
+};
 export const MainLayoutWrapper: FC<TMainLayoutWrapperProps> = ({children}) => {
   return (
     <div>
       <Button
         text={'Тест'}
         onClick={(e) => {
-
+          const currentTheme = DevextremeUiThemes.current();
+          DevextremeUiThemes.current(themeMap[currentTheme]);
         }}
       />
+
       {children}
     </div>
   );
